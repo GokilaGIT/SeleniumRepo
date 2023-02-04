ad0d3494c4bbaa356dd04c28352e04813fb9fca5
GokilaGIT <124426893+GokilaGIT@users.noreply.github.com>
2023-02-04 21:24:07 +0530

    Gokila week1 day1 selenium

---
+++ week1/day1/Mobile.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Mobile {
+public static void main(String[] args) {
+	
+	
+	String mobileBrandName = "Samsung";
+	char mobileLogo = '$';
+	short noOfMobilePiece = 2;
+	int modelNumber = 21;
+	long mobilemeiNumber = 23343434343l;
+	float mobilePrice =39999.99f;
+	boolean isDamaged = false;
+	
+	System.out.println(mobileBrandName);
+	System.out.println(mobileLogo);d
+	System.out.println(noOfMobilePiece);
+	System.out.println(modelNumber);
+	System.out.println(mobilePrice);
+	System.out.println(isDamaged);
+	
+	
+	
+	
+}
+}
---
+++ week1/day1/TwoWheeler.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Gokilavani");
+		
+		int noOfWheels = 2;			
+		short noOfMirrors = -3;			
+		long chassisNumber = 982343434343l;
+		boolean isPunctured = true;		
+		String bikeName = "Honda";		
+		double runinKM = 89.95;
+		
+		
+		System.out.println(noOfWheels);
+		System.out.println(noOfMirrors);
+		System.out.println(chassisNumber);
+		System.out.println(isPunctured);
+		System.out.println(bikeName);
+		System.out.println(runinKM);
+		
+		
+	}
+}
